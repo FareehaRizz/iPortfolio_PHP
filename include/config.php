42aec3a3208f55b0b1cbfdb6e6a6a3022f880643
PC Mart <PC Mart@DESKTOP-7TC8DIC>
2024-03-09 00:41:48 +0500

    Add project files.

---
+++ include/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "iportfolio";
+
+//connecting the mySQL databse with php
+
+$con = mysqli_connect($db_host , $db_user , $db_pass , $db_name);
+
+?>
